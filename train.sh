cfc62f629cfab3ece1f2c4ed6473cab46c130178
Lucas Werkmeister <mail@lucaswerkmeister.de>
2015-12-17 00:29:12 +0100

    Use lowercase variable names
    Uppercase variabes are conventionally reserved for program environment
    configuration (MAKEFLAGS, LESSOPEN) and shell variables (USER, IFS).
    Using uppercase for regular variables risks unintentionally overriding
    these variables, which especially for shell variables can be
    problematic.

--- train.sh
+++ train.sh
@@ -10,13 +10,13 @@ while true; do
         echo finished
         exit
     fi
-    LINE=$(shuf -n 1 words.txt)
+    line=$(shuf -n 1 words.txt)
     IFS=";"
-    PARTS=($LINE)
-    cnt=$(grep -c '^'"${PARTS[0]}"'$' success)
-    cntm=$(grep -c '^'"${PARTS[0]}"'$' fail)
+    parts=($line)
+    cnt=$(grep -c '^'"${parts[0]}"'$' success)
+    cntm=$(grep -c '^'"${parts[0]}"'$' fail)
     if [[ $((cnt-cntm)) -gt 2 ]]; then
-        #echo "Skipped: ${PARTS[0]}"
+        #echo "Skipped: ${parts[0]}"
         continue;
     fi
     echo $cnt"; Required: "$((cntm+3))
@@ -24,24 +24,24 @@ while true; do
 
     failed="NO"
     while true; do
-        read -r -p "${PARTS[1]}: " ENTRY
-        if [[ "${PARTS[0]}" == "$ENTRY" ]]; then
+        read -r -p "${parts[1]}: " entry
+        if [[ "${parts[0]}" == "$entry" ]]; then
             tput setaf 2
             echo Success
             tput setaf 7
             if [[ $failed == "NO" ]]; then
-                echo "${PARTS[0]}" >> success
+                echo "${parts[0]}" >> success
             fi
             break;
         else
             tput setaf 1
             echo -n "Fail: "
             tput setaf 2
-            echo "${PARTS[0]}"
+            echo "${parts[0]}"
             tput setaf 7
             if [[ $failed == "NO" ]]; then
                 failed="YES"
-                echo "${PARTS[0]}" >> fail
+                echo "${parts[0]}" >> fail
             fi
         fi
     done
