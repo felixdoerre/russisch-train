c49c714d6956b9fdd23b0cbd1537cdd9394000cc
Lucas Werkmeister <mail@lucaswerkmeister.de>
2015-12-17 00:44:58 +0100

    Extract tput result into variables
    This should make the code using it easier to read: you no longer have to
    remember which color ASCII escapes 1, 2, and 7 stand for.
    
    (It is also a very slight performance improvement, since tput is only
    called three times, not several times per iteration.)

--- train.sh
+++ train.sh
@@ -5,6 +5,10 @@ cd $1
 touch success
 touch fail
 
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+white="$(tput setaf 7)"
+
 while true; do
     if diff -q <(sort <(cat words.txt words.txt words.txt| cut -f1 -d\;) fail) <(sort success) > /dev/null; then
         echo finished
@@ -23,19 +27,13 @@ while true; do
     while true; do
         read -r -p "$de: " entry
         if [[ "$ru" == "$entry" ]]; then
-            tput setaf 2
-            echo Success
-            tput setaf 7
+            echo "${green}Success${white}"
             if [[ $failed == "NO" ]]; then
                 echo "$ru" >> success
             fi
             break;
         else
-            tput setaf 1
-            echo -n "Fail: "
-            tput setaf 2
-            echo "$ru"
-            tput setaf 7
+            echo "${red}Fail: ${green}$ru${white}"
             if [[ $failed == "NO" ]]; then
                 failed="YES"
                 echo "$ru" >> fail
